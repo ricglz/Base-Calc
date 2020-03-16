f694a70ed091e3372d7163495e1b003f70c68b8f
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-03-15 18:45:12 -0600

    Initial Commit

---
+++ BaseCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaseCalc
+//
+//  Created by Ricardo J. González on 15/03/20.
+//  Copyright © 2020 The Senate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
