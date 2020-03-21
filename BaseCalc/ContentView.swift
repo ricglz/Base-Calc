a74340c95911125e6b3af4f6ac670949bb524bb4
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-03-21 15:23:19 -0600

    KeypadButtons now perform an action

--- BaseCalc/ContentView.swift
+++ BaseCalc/ContentView.swift
@@ -107,11 +107,13 @@ struct KeypadButton: View {
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B",
         "C", "D", "E", "F"
     ]
+    
     @EnvironmentObject var calculatorState: CalculatorState
 
     var body: some View {
-        let enabled = value() < calculatorState.currentBase.rawValue
-        return Button(action: {}) {
+        let isAnOperationBtn = operationBtns.contains(label)
+        let enabled = value(isAnOperationBtn) < calculatorState.currentBase.rawValue
+        return Button(action: btnAction(isAnOperationBtn)) {
             Text(label)
                 .font(.title)
                 .foregroundColor(enabled ? .white : .gray)
@@ -129,9 +131,66 @@ struct KeypadButton: View {
             return enabled ? Color.enabledDigit : Color.disabledDigit
         }
     }
+    
+    func btnAction(_ isAnOperationBtn: Bool) -> () -> Void {
+        isAnOperationBtn ? operationAction() : digitAction
+    }
+    
+    func operationAction() -> () -> Void {
+        switch label {
+        case "AC":
+            return clearAction
+        case "±":
+            return changeSign
+        case "+":
+            return sum
+        case "-":
+            return substract
+        case "ß":
+            return baseComplement
+        case "=":
+            return solve
+        case ".":
+            return addDecimalDot
+        default:
+            return { print(self.label) }
+        }
+    }
+    
+    func digitAction() {
+        print(label)
+    }
+    
+    func clearAction() {
+        print("Clearing")
+    }
+    
+    func changeSign() {
+        print("Changing sign")
+    }
+    
+    func sum() {
+        print("Suming")
+    }
+    
+    func substract() {
+        print("Substraction")
+    }
+    
+    func baseComplement() {
+        print("Performing base complement")
+    }
+    
+    func solve() {
+        print("Solving problem")
+    }
+    
+    func addDecimalDot() {
+        print("Adding decimal dot")
+    }
 
-    func value() -> Int {
-        operationBtns.contains(label) ? 0 : digits.firstIndex(of: label)!
+    func value(_ isAnOperationBtn: Bool) -> Int {
+        isAnOperationBtn ? 0 : digits.firstIndex(of: label)!
     }
 }
 
