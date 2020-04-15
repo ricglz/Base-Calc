a1c16067359d7e7191a458ed7b5baa83d95e04d1
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-04-14 23:41:05 -0500

    Add test for changing base operation for calculator state

---
+++ BaseCalcTests/CalculatorStateTests.swift
@@ -0,0 +1,37 @@
+//
+//  CalculatorStateTests.swift
+//  BaseCalcTests
+//
+//  Created by Ricardo J. González on 14/04/20.
+//  Copyright © 2020 The Senate. All rights reserved.
+//
+
+import XCTest
+
+@testable import BaseCalc
+
+class CalculatorStateTests: XCTestCase {
+    
+    private var state: CalculatorState!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        state = CalculatorState()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
+        state = nil
+    }
+    
+    //MARK:- Change Base Operation
+
+    func testWholeNumberWithPointChangeBase() throws {
+        state.currentText = "10."
+        state.currentBase = .Base10
+        state.changeBase(.Base4)
+        XCTAssertEqual(state.currentText, "22")
+    }
+
+}
